46238a9e82abb0841dc9d0d9ca07b8654fc8fdfe
andrec <andrec@iastate.edu>
2018-09-12 15:49:55 -0500

    Test for writing SQL queries in Java

--- experiments/andre/experiment/src/experiment/Java_sql.java
+++ experiments/andre/experiment/src/experiment/Java_sql.java
@@ -1,7 +1,30 @@
-package experiment;
-
+package experimental_server.demo;
+import org.springframework.*;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+ 
+import javax.sql.DataSource;
+ 
+@Configuration
 public class Java_sql {
-	public static void main(String args[]) {
-		System.out.println("Hello World");
-	}
+ 
+    private static final String QUERY_FIND_RESIDENTS =
+            "SELECT " +
+                "name, " +
+                "email_address, " +
+                "street_address " +
+            "FROM RESIDENTS " +
+            "ORDER BY street_address ASC";
+ 
+    @Bean
+    ItemReader<Resident> databaseXmlItemReader(DataSource dataSource) {
+        JdbcCursorItemReader<Residents> databaseReader = new JdbcCursorItemReader<>();
+ 
+        databaseReader.setDataSource(dataSource);
+        databaseReader.setSql(QUERY_FIND_RESIDENTS);
+        databaseReader.setRowMapper(new BeanPropertyRowMapper<>(Residents.class));
+ 
+        return databaseReader;
+    }
 }

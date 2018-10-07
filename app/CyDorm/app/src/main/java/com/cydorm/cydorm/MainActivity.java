30c7e126427d845f0ff9952f19048f891d78718f
Malcolm Boyd <mjboyd@iastate.edu>
2018-10-07 13:16:29 -0500

    Write empty home view
    Wrote a home view that can be used to link to other views. This is done
    with a Recyclerview and a Card layout. A card needs to be made for each
    card item, this can be added to the Recycler View.

--- app/CyDorm/app/src/main/java/com/cydorm/cydorm/MainActivity.java
+++ app/CyDorm/app/src/main/java/com/cydorm/cydorm/MainActivity.java
@@ -2,12 +2,36 @@ package com.cydorm.cydorm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.cardList);
+
+        //TODO this may need to change with time
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        List<ContactInfo> templist = new ArrayList<>();
+        templist.add(new ContactInfo());
+        templist.add(new ContactInfo());
+        templist.add(new ContactInfo());
+        templist.add(new ContactInfo());
+        mAdapter = new ContactAdapter(templist);
+
+        mRecyclerView.setAdapter(mAdapter);
     }
 }

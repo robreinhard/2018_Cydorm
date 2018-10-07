e0b336db82b222d715896b723a565f3bf35040c6
Malcolm Boyd <mjboyd@iastate.edu>
2018-10-07 13:16:29 -0500

    Write empty home view
    Wrote a home view that can be used to link to other views. This is done
    with a Recyclerview and a Card layout. A card needs to be made for each
    card item, this can be added to the Recycler View.

---
+++ app/CyDorm/app/src/main/java/com/cydorm/cydorm/ContactInfo.java
@@ -0,0 +1,10 @@
+package com.cydorm.cydorm;
+
+public class ContactInfo {
+    protected String name = "Malc";
+    protected String surname="Talk";
+    protected String email = "@alk";
+    protected static final String NAME_PREFIX = "Name_";
+    protected static final String SURNAME_PREFIX = "Surname_";
+    protected static final String EMAIL_PREFIX = "email_";
+}

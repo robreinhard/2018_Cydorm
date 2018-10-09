34ab7bc3416cba8cced8869da1afcf07ad9827ac
Robert Reinhard <robgreinhard@gmail.com>
2018-10-09 16:31:40 -0500

    now syncs groceries

--- Backend/com-s-309-project/bin/main/static/javascript/navbar.js
+++ Backend/com-s-309-project/bin/main/static/javascript/navbar.js
@@ -7,6 +7,65 @@ var btn;
 var span;
 
 // When the user clicks the button, open the modal 
+var groceries;
+var pendingGroceries = [];
+var purchaseGroceries = [];
+
+window.onload = function() {
+
+    setInterval(function(){
+
+	var requestURL = 'http://localhost:8080/allGroceries';
+	    var request = new XMLHttpRequest();
+	    request.open('GET', requestURL);
+	    request.responseType = 'json';
+	    request.send();
+	
+	    request.onload = function() {
+	    	
+	    	  groceries = request.response;
+	    	  for (var i=0; i < groceries.length;i++) {
+	    		  
+	    		  if (groceries[i].approval === 'F') {
+	    			  
+	    			  var isNotPushed = true;
+	    			  for (var j=0; j < purchaseGroceries.length;j++) {
+
+	    				  if (groceries[i].id == purchaseGroceries[j].id) {
+	    					  isNotPushed = false;
+	    				  }
+	    				  
+	    			  }
+	    			  if (isNotPushed) {
+	    				  
+	    				  purchaseGroceries.push(groceries[i]);
+	    				  $( "#toPurchase" ).append('<div class="groceryItemContainer" id=' + groceries[i].id + '><h4 class="containerComponents">' + groceries[i].groceryItem + '<h4 class="containerComponents">$'+ groceries[i].groceryPrice+'</h4><h4 class="containerComponents">'+groceries[i].firstName+'</h4><input type="checkbox" class="containerComponents floatRight"></div>');
+
+	    			  }
+	    		  }
+	    		  else {
+	    			  
+	    			  var isNotPushed = true;
+	    			  for (var j=0; j < pendingGroceries.length;j++) {
+	    				  
+	    				  if (groceries[i].id == pendingGroceries[j].id) {
+	    					  
+	    					  isNotPushed = false;
+	    				  }
+	    			  }
+	    			  if (isNotPushed) {
+	    				  
+	    				  pendingGroceries.push(groceries[i]);
+	    				  $( "#pendingPurchase" ).append('<div class="groceryItemContainer" id=' + groceries[i].id + '><h4 class="containerComponents">' + groceries[i].groceryItem + '<h4 class="containerComponents">$'+ groceries[i].groceryPrice+'</h4><h4 class="containerComponents">'+groceries[i].firstName+'</h4><input type="checkbox" checked="checked" class="containerComponents floatRight"></div>');
+
+	    			  }
+	    		  }
+	
+	    	  }
+		}
+    }, 1000);
+    
+};
 
 
 function theTest() {
@@ -15,16 +74,6 @@ function theTest() {
 	btn = document.getElementById("myBtn");
 	document.getElementsByClassName("close")[0];
     modal.style.display = "block";
-    var requestURL = 'http://proj309-vc-05.misc.iastate.edu:8080/allUsers';
-    var request = new XMLHttpRequest();
-    request.open('GET', requestURL);
-    request.responseType = 'json';
-    request.send();
-    var superHeroes;
-    request.onload = function() {
-    	  superHeroes = request.response;
-    	  console.log(superHeroes);
-	}
     
 }
 
@@ -34,6 +83,9 @@ function closed() {
 	document.getElementById('gItem1').value = '';
 	document.getElementById('gItem2').value = '';
  	document.getElementById('gItem3').value = '';
+ 	document.getElementById('pItem1').value = '';
+	document.getElementById('pItem2').value = '';
+ 	document.getElementById('pItem3').value = '';
 	document.getElementById("gCheck1").checked = false;
 	document.getElementById("gCheck2").checked = false;
 	document.getElementById("gCheck3").checked = false;
@@ -43,47 +95,49 @@ function closed() {
 
 function addGroceryItems() {
 	
-	var item1Text = document.getElementById('gItem1').value;
-	var item2Text = document.getElementById('gItem2').value;
-	var item3Text = document.getElementById('gItem3').value;
+	var item1Text = document.getElementById('gItem1').value.toString();
+	var item2Text = document.getElementById('gItem2').value.toString();
+	var item3Text = document.getElementById('gItem3').value.toString();
+	var price1 = document.getElementById('pItem1').value;
+	var price2 = document.getElementById('pItem2').value;
+	var price3 = document.getElementById('pItem3').value;
 	var check1 = document.getElementById("gCheck1").checked;
 	var check2 = document.getElementById("gCheck2").checked;
 	var check3 = document.getElementById("gCheck3").checked;
-
-	if (item1Text.length != 0) {
+	if (item1Text.length != 0 && !isNaN(price1)) {
 		
 		if (check1 == true) {
 			
-			$( "#pendingPurchase" ).append('<div class="groceryItemContainer"><h4 class="containerComponents">' + item1Text+ '</h4> <input type="checkbox" checked="checked" class="containerComponents floatRight"></div>');
-
+			var string = "/addGroceryItem?groceryItem=Mustard&groceryPrice=2.35&approved=T&firstName=Andre&lastName=Chickering";
+			
 		}
 		else {
-			$( "#toPurchase" ).append('<div class="groceryItemContainer"><h4 class="containerComponents">' + item1Text+ '</h4> <input type="checkbox" class="containerComponents floatRight"></div>');
 
+			
 		}
 	}
-	if (item2Text.length != 0) {
+	if (item2Text.length != 0 && !isNaN(price2)) {
 		
 		if (check2 == true) {
 			
-			$( "#pendingPurchase" ).append('<div class="groceryItemContainer"><h4 class="containerComponents">' + item2Text+ '</h4> <input type="checkbox" checked="checked" class="containerComponents floatRight"></div>');
 
+			
 		}
 		else {
-			$( "#toPurchase" ).append('<div class="groceryItemContainer"><h4 class="containerComponents">' + item2Text+ '</h4> <input type="checkbox" class="containerComponents floatRight"></div>');
 
+			
 		}
 	}
-	if (item3Text.length != 0) {
+	if (item3Text.length != 0 && !isNaN(price3)) {
 		
 		if (check3 == true) {
 			
-			$( "#pendingPurchase" ).append('<div class="groceryItemContainer"><h4 class="containerComponents">' + item3Text+ '</h4> <input type="checkbox" checked="checked" class="containerComponents floatRight"></div>');
-
+			
+			
 		}
 		else {
-			$( "#toPurchase" ).append('<div class="groceryItemContainer"><h4 class="containerComponents">' + item3Text+ '</h4> <input type="checkbox" class="containerComponents floatRight"></div>');
 
+			
 		}
 	}	
 

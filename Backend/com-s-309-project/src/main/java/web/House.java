56155a0c01041e6b0af0ba9f08e01d7dcd6b1fc8
andrec <andrec@iastate.edu>
2018-10-29 00:28:06 -0500

    Entity for user's house

--- Backend/com-s-309-project/src/main/java/web/House.java
+++ Backend/com-s-309-project/src/main/java/web/House.java
@@ -6,7 +6,37 @@ import javax.persistence.*;
 public class House {
 	
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	
+	@Column
 	private String name;
+	@Column
+	private String resident1;
+	@Column
+	private String resident2;
+	@Column
+	private String resident3;
+	@Column
+	private String resident4;
+	@Column
+	private String chores;
+	
+	public House(String name, String resident1, String resident2, String resident3, String resident4) {
+		this.name = name;
+		this.resident1 = resident1;
+		this.resident2 = resident2;
+		this.resident3 = resident3;
+		this.resident4 = resident4;
+	}
+	
+	
+	public void setChoreList(String chores) {
+		this.chores = chores;
+	}
+	
+	//This could be a relationship
+	public String chores() {
+		return chores;
+	}
 }

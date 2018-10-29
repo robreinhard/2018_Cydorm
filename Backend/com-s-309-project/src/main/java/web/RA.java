c1b25b86a204545b72c8b2a88a8122493ac955f6
andrec <andrec@iastate.edu>
2018-10-29 00:28:32 -0500

    Entity for the CA/RA user

--- Backend/com-s-309-project/src/main/java/web/RA.java
+++ Backend/com-s-309-project/src/main/java/web/RA.java
@@ -1,5 +1,16 @@
 package web;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+import javax.persistence.Column;
+
+
+@Entity
+@Table(name = "Ras")
 public class RA {
+	
 	
 }

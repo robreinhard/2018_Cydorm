8b9f89903f0baa60ec1cca8e208ad601c1fbc4e3
andrec <andrec@iastate.edu>
2018-11-05 01:14:12 -0600

    Fixed bug that affected Android websocket compatibility

--- Backend/com-s-309-project/bin/main/static/javascript/navbar.js
+++ Backend/com-s-309-project/bin/main/static/javascript/navbar.js
@@ -8,108 +8,207 @@ var span;
 
 // When the user clicks the button, open the modal 
 var groceries;
+var chores;
 var pendingGroceries = [];
 var purchaseGroceries = [];
+var toDoChores = [];
+var pendingChores = [];
+var stompClient = null;
 
 window.onload = function() {
-
-    setInterval(function(){
-
-    	var requestURL = 'http://proj309-vc-05.misc.iastate.edu:8080/allGroceries';
-	    var request = new XMLHttpRequest();
-	    request.open('GET', requestURL);
-	    request.responseType = 'json';
-	    request.send();
 	
-	    request.onload = function() {
-	    	
-	    	  groceries = request.response;
-	    	  for (var i=0; i < groceries.length;i++) {
-	    		  
-	    		  if (groceries[i].approval === 'F') {
-	    			  
-	    			  var isNotPushed = true;
-	    			  for (var j=0; j < purchaseGroceries.length;j++) {
+	var socket = new SockJS('/gs-guide-websocket');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/allGroceries', function(theList) {
+        	updateGroceryList(JSON.parse(theList.body));
+        });
+        stompClient.subscribe('/allChores', function(theChores) {
+        	
+        	updateChoresList(JSON.parse(theChores.body));
+        });
+    	stompClient.send("/dumpGrocery",{},JSON.stringify({'netID': netID}));
+    	stompClient.send("/dumpChore",{},JSON.stringify({'netID': netID}));
 
-	    				  if (groceries[i].id == purchaseGroceries[j].id) {
-	    					  isNotPushed = false;
-	    				  }
-	    				  
-	    			  }
-	    			  if (isNotPushed) {
-	    				  
-	    				  purchaseGroceries.push(groceries[i]);
-	    				  $( "#toPurchase" ).append('<div class="groceryItemContainer" id=' + groceries[i].id + ' onclick="modifyRemovePrompt('+ groceries[i].id + ')"><h4 class="containerComponents">' + groceries[i].groceryItem + '</h4><h4 class="containerComponents">$'+ groceries[i].groceryPrice+'</h4><h4 class="containerComponents">'+groceries[i].firstName+'</h4><input type="checkbox" class="containerComponents floatRight"></div>');
+    });
+};   
 
-	    			  }
-	    		  }
-	    		  else {
-	    			  
-	    			  var isNotPushed = true;
-	    			  for (var j=0; j < pendingGroceries.length;j++) {
-	    				  
-	    				  if (groceries[i].id == pendingGroceries[j].id) {
-	    					  
-	    					  isNotPushed = false;
-	    				  }
-	    			  }
-	    			  if (isNotPushed) {
-	    				  
-	    				  pendingGroceries.push(groceries[i]);
-	    				  $( "#pendingPurchase" ).append('<div class="groceryItemContainer" id=' + groceries[i].id + ' onclick="modifyRemovePrompt('+ groceries[i].id + ')"><h4 class="containerComponents">' + groceries[i].groceryItem + '</h4><h4 class="containerComponents">$'+ groceries[i].groceryPrice+'</h4><h4 class="containerComponents">'+groceries[i].firstName+'</h4><input type="checkbox" checked="checked" class="containerComponents floatRight"></div>');
-
-	    			  }
-	    		  }
+function updateChoresList(chores) {
 	
-	    	  }
-	    	 
-	    	  /*
-	    	  for (var k=0; k < toBeRemoved.length;k++) {
-	    		  
-				  $( "#" + toBeRemoved[k].id).remove();
-	    	  }*/
+	for (var i=0; i < chores.length;i++) {
+
+		if (chores[i].completed === 'F') {
+			  
+		  var isNotPushed = true;
+		  for (var j=0; j < toDoChores.length;j++) {
+
+			  if (chores[i].chore_id == toDoChores[j].chore_id) {
+				  
+				  isNotPushed = false;
+			  }
+				  
+		  }
+		  if (isNotPushed) {
+				  
+			  toDoChores.push(chores[i]);
+			  var date = new Date(chores[i].dueDate);
+			  
+			  $( "#toDoChores" ).append('<div class="groceryItemContainer" id=c' + chores[i].chore_id + ' onclick="modifyRemovePromptChore('+ chores[i].chore_id + ')"><h4 class="containerComponents">' + chores[i].chore + '</h4><h4 class="containerComponents">'+ (date.getMonth()+1) + '/' + date.getDate()+'</h4><h4 class="containerComponents">'+chores[i].studentID+'</h4><input type="checkbox" class="containerComponents floatRight"></div>');
+
+		  }
+		}
+		else {
+			  
+		  var isNotPushed = true;
+		  for (var j=0; j < pendingChores.length;j++) {
+				  
+			  if (chores[i].chore_id == pendingChores[j].chore_id) {
+					  
+			  	isNotPushed = false;
+			  	
+			  }
+		  }
+		  if (isNotPushed) {
+				  
+			  	pendingChores.push(chores[i]);
+			  	var date = new Date(chores[i].dueDate);
+
+			  	$( "#pendingChores" ).append('<div class="groceryItemContainer" id=c' + chores[i].chores_id + ' onclick="modifyRemovePromptChore('+ chores[i].chore_id + ')"><h4 class="containerComponents">' + chores[i].chore + '</h4><h4 class="containerComponents">'+ (date.getMonth()+1) + '/' + date.getDate()+'</h4><h4 class="containerComponents">'+chores[i].studentID+'</h4><input type="checkbox" checked="checked" class="containerComponents floatRight"></div>');
+
+		  }
+		}
+
+		var allChores = pendingChores.concat(toDoChores);
+		console.log(allChores);
+		  var toBeRemoved = allChores.filter(function(g) {
+
+			for (var k=0; k < chores.length;k++) {
+
+			  if (chores[k].chore_id == g.chore_id) {
+				  console.log(chores[k].chore_id + ":" + g.chore_id);
+				  return false;
+			  }
+			  
+			}
+			console.log("runs");
+			return true;
+		  
+		  });
+		  console.log("hello");
+		  console.log(toBeRemoved);
+			
+		  for (var l=0; l < toBeRemoved.length;l++) {
+			console.log(toBeRemoved[l]);
+			if (toBeRemoved.length > 0) {
+				
+				var toRemove = "#c" + toBeRemoved[l].chore_id.toString();
+				console.log(toRemove);
+				$(toRemove).remove();
+			}
+			
+
+			
+		  }
+}
+}
+
+function updateGroceryList(groceries) {
+	
+	for (var i=0; i < groceries.length;i++) {
+		console.log(groceries[i].approval);
+		if (groceries[i].approval === 'F') {
+			  
+		  var isNotPushed = true;
+		  for (var j=0; j < purchaseGroceries.length;j++) {
+
+			  if (groceries[i].id == purchaseGroceries[j].id) {
+				  
+				  isNotPushed = false;
+			  }
+				  
+		  }
+		  if (isNotPushed) {
+				  
+			  purchaseGroceries.push(groceries[i]);
+			  $( "#toPurchase" ).append('<div class="groceryItemContainer" id=g' + groceries[i].id + ' onclick="modifyRemovePromptGrocery('+ groceries[i].id + ')"><h4 class="containerComponents">' + groceries[i].groceryItem + '</h4><h4 class="containerComponents">$'+ groceries[i].groceryPrice+'</h4><h4 class="containerComponents">'+groceries[i].studentID+'</h4><input type="checkbox" class="containerComponents floatRight"></div>');
+
+		  }
+		}
+		else {
+			  
+		  var isNotPushed = true;
+		  for (var j=0; j < pendingGroceries.length;j++) {
+				  
+			  if (groceries[i].id == pendingGroceries[j].id) {
+					  
+			  	isNotPushed = false;
+			  	
+			  }
+		  }
+		  if (isNotPushed) {
+				  
+			  	pendingGroceries.push(groceries[i]);
+			  	$( "#pendingPurchase" ).append('<div class="groceryItemContainer" id=g' + groceries[i].id + ' onclick="modifyRemovePromptGrocery('+ groceries[i].id + ')"><h4 class="containerComponents">' + groceries[i].groceryItem + '</h4><h4 class="containerComponents">$'+ groceries[i].groceryPrice+'</h4><h4 class="containerComponents">'+groceries[i].studentID+'</h4><input type="checkbox" checked="checked" class="containerComponents floatRight"></div>');
+
+		  }
+		}
+
+  }
+	 
+  var allGroceries = pendingGroceries.concat(purchaseGroceries);
+
+  var toBeRemoved = allGroceries.filter(function(g) {
+
+	for (var k=0; k < groceries.length;k++) {
+
+	  if (groceries[k].id == g.id) {
+		  
+		  return false;
+	  }
+	  
+	}
+  
+	return true;
+  
+  });
+  
+  console.log(toBeRemoved);
+	
+  for (var l=0; l < toBeRemoved.length;l++) {
+	
+	if (toBeRemoved.length > 0) {
 		
-	    
-	    var allGroceries = pendingGroceries.concat(purchaseGroceries);
-  	  
-  	  	var toBeRemoved = allGroceries.filter(function(g) {
-  		
-  		  for (var k=0; k < groceries.length;k++) {
-  			  
-  			  if (groceries[k].id == g.id) {
-  				  
-  				  return false;
-  			  }
-  			  
-  		  }
-  		  
-  		  return true;
-  	  	});
-  	  	console.log(toBeRemoved);
-  	  	
-  	  	for (var l=0; l < toBeRemoved.length;l++) {
-  	  	
-  	  		if (toBeRemoved.length > 0) {
-  	  			var toRemove = "#" + toBeRemoved[l].id.toString();
-  	  			$(toRemove).remove();
-  	  		}
-  	  	
-    	}
-  	  	
-	    }
-    }, 1000);
-    
-};
-
-
-function theTest() {
+		var toRemove = "#g" + toBeRemoved[l].id.toString();
+		$(toRemove).remove();
+	}
 	
-	modal = document.getElementById('myModal');
+
+	
+  }
+	
+}
+
+
+
+function groceryAdd() {
+	
+	modal = document.getElementById('groceryAddModal');
 	btn = document.getElementById("myBtn");
 	document.getElementsByClassName("close")[0];
     modal.style.display = "block";
     
 }
+
+function choreAdd() {
+	
+	modal = document.getElementById('choreAddModal');
+	btn = document.getElementById("myBtn");
+	document.getElementsByClassName("close")[0];
+    modal.style.display = "block";
+    
+}
+
 
 // When the user clicks on <span> (x), close the modal
 function closed() {
@@ -173,20 +272,22 @@ function addGroceryItems() {
 		
 		item1.groceryItem = item1Text;
 		item1.groceryPrice = price1;
-		
+		var approved;
 		
 		if (check1 == true) {
 			
 			
-			item1.approved = 'T';	
+			approved = 'T';	
 		}
 		else {
 
-			item1.approved = 'F';	
+			approved = 'F';	
 
 		}
 		
+		stompClient.send("/addGroceryItem",{},JSON.stringify({'groceryItem': item1Text,'groceryPrice': price1,'approved' : approved,'studentID': netID}));
 		
+		/*
 		var str = [];
 		for (var p in item1)
 		if (item1.hasOwnProperty(p)) {
@@ -203,26 +304,27 @@ function addGroceryItems() {
 	    	
 	    	  console.log("Success");
 	    	  
-		}
+		}*/
 	}
 	if (item2Text.length != 0 && !isNaN(price2)) {
 		
 		item2.groceryItem = item2Text;
 		item2.groceryPrice = price2;
-		
+		var approved;
+
 		if (check2 == true) {
 			
-			item2.approved = 'T';	
+			approved = 'T';	
 
 			
 		}
 		else {
 
-			item2.approved = 'F';	
+			approved = 'F';	
 
 			
 		}
-		
+		/*
 		var str = [];
 		for (var p in item2)
 		if (item2.hasOwnProperty(p)) {
@@ -239,25 +341,30 @@ function addGroceryItems() {
 	    	
 	    	  console.log("Success");
 	    	  
-		}
+		}*/
+		
+		stompClient.send("/addGroceryItem",{},JSON.stringify({'groceryItem': item2Text,'groceryPrice': price2,'approved' : approved,'studentID': netID}));
+
 	}
 	if (item3Text.length != 0 && !isNaN(price3)) {
 		
 		item3.groceryItem = item3Text;
 		item3.groceryPrice = price3;	
+		var approved; 
 		
 		if (check3 == true) {
 			
-			item3.approved = 'T';	
+			approved = 'T';	
 			
 			
 		}
 		else {
 
-			item3.approved = 'F';	
+			approved = 'F';	
 
 			
 		}
+		/*
 		var str = [];
 		for (var p in item3)
 		if (item3.hasOwnProperty(p)) {
@@ -274,16 +381,18 @@ function addGroceryItems() {
 	    	
 	    	  console.log("Success");
 	    	  
-		}
+		}*/
+		stompClient.send("/addGroceryItem",{},JSON.stringify({'groceryItem': item3Text,'groceryPrice': price3,'approved' : approved,'studentID': netID}));
+	
 	}	
 
 	closed();
 }
 var result;
 
-function modifyRemovePrompt(id) {
+function modifyRemovePromptGrocery(id) {
 	
-	modal = document.getElementById('myModal2');
+	modal = document.getElementById('groceryEditModal');
 	btn = document.getElementById("myBtn");
 	document.getElementsByClassName("close")[0];
     modal.style.display = "block";
@@ -295,23 +404,14 @@ function modifyRemovePrompt(id) {
 	document.getElementById('gPrice').value = result.groceryPrice;
 }
 
-function toDelete() {
+function toDeleteGrocery() {
 	
-	var url = 'http://proj309-vc-05.misc.iastate.edu:8080/deleteGroceryItem?id=' + result.id;
-	
-    var request = new XMLHttpRequest();
-    request.open('GET', url);
-    request.send();
+	stompClient.send("/deleteGroceryItem",{},JSON.stringify({'netID': netID,'grocery_id':result.id}));
 
-    request.onload = function() {
-    	
-    	  console.log("Success");
-    	  
-	}
     closed();
 }
 
-function modify() {
+function modifyGrocery() {
 	
 	
 	var newItem =  document.getElementById('gItem').value;
@@ -319,33 +419,70 @@ function modify() {
 	var url = 'http://proj309-vc-05.misc.iastate.edu:8080/addGroceryItem?';
 	
 	if (newItem.length != 0 && !isNaN(newPrice) && !(newItem==result.groceryItem && newPrice==result.groceryPrice)) {
-		toDelete();
+		toDeleteGrocery();
 	
 		result.groceryItem = newItem;
 		result.groceryPrice = newPrice;
 		
 		var str = [];
-		
-		str.push(encodeURIComponent("groceryItem") + "=" + encodeURIComponent(newItem));
-		str.push(encodeURIComponent("groceryPrice") + "=" + encodeURIComponent(newPrice));
-		str.push(encodeURIComponent("approved") + "=" + encodeURIComponent(result.approval));
-		str.push(encodeURIComponent("firstName") + "=" + encodeURIComponent(result.firstName));
-		str.push(encodeURIComponent("lastName") + "=" + encodeURIComponent(result.lastName));
+		stompClient.send("/addGroceryItem",{},JSON.stringify({'groceryItem': newItem,'groceryPrice': newPrice,'approved' : result.approval,'studentID': netID}));
 
-		var encoded =  str.join("&");
-		console.log(encoded);
-		var requestURL = url + encoded;
-	    var request = new XMLHttpRequest();
-	    request.open('GET', requestURL);
-	    request.send();
-	
-	    request.onload = function() {
-	    	
-	    	  console.log("Success");
-	    	  
-		}
+		
 	    
 	}
 }
 
+function modifyRemovePromptChore(id) {
+	
+	modal = document.getElementById('choreEditModal');
+	btn = document.getElementById("myBtn");
+	document.getElementsByClassName("close")[0];
+    modal.style.display = "block";
+    var allChores = pendingChores.concat(toDoChores);
+    result = allChores.find(obj => {
+    	  return obj.chore_id === id
+	})
+	console.log(result);
+    document.getElementById('cItemMod').value = result.chore;
+
+}
+
+function toDeleteChore() {
+	
+	stompClient.send("/deleteChoreItem",{},JSON.stringify({'netID': netID,'chore_id':result.chore_id}));
+
+    closed();
+}
+
+function modifyChore() {
+	
+	console.log("RUN");
+	var newChore =  document.getElementById('cItemMod').value;
+	var newDueDate = document.getElementById('dateItemMod').value;
+	console.log(newDueDate);
+	if (newChore.length != 0 && !isNaN(newDueDate)) {
+		
+		toDeleteChore();
+	
+		
+		var str = [];
+		stompClient.send("/addChore",{},JSON.stringify({'cItem': newChore,'nextSevenDays' : nextSevenDays[newDueDate],'correspondingMonth':correspondingMonth[newDueDate],'correspondingYear':correspondingYear[newDueDate],'studentID': netID}));
+
+		
+	    
+	}
+
+}
+
 // When the user clicks anywhere outside of the modal, close it
+function addChore() {
+	
+	var cItem = document.getElementById('cItem').value.toString();
+	var dateItem = document.getElementById('dateItem').value.toString();
+	if (cItem.length != 0 && !isNaN(dateItem)) {
+	
+		stompClient.send("/addChore",{},JSON.stringify({'cItem': cItem,'nextSevenDays' : nextSevenDays[dateItem],'correspondingMonth':correspondingMonth[dateItem],'correspondingYear':correspondingYear[dateItem],'studentID': netID}));
+	
+	}
+	closed();
+}

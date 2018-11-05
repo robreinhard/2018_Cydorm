a8ff6400b42f2472a47263f1289ea970266bc3aa
Malcolm Boyd <mjboyd@iastate.edu>
2018-11-05 01:28:40 -0600

    Initial android chore work

---
+++ app/CyDorm/app/src/main/java/com/cydorm/cydorm/AddChoreActivity.java
@@ -0,0 +1,53 @@
+package com.cydorm.cydorm;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import android.support.v4.app.DialogFragment;
+import java.util.Calendar;
+
+public class AddChoreActivity extends AppCompatActivity {
+
+    EditText dateText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_chore);
+
+        dateText = (EditText) findViewById(R.id.chore_date_picker);
+        dateText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment newFragment = new DatePickerFragment();
+                newFragment.show(getSupportFragmentManager(), "datePicker");
+            }
+        });
+
+
+    }
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+            DatePickerDialog dialog = new DatePickerDialog(getActivity(), this, year, month, day);
+            dialog.getDatePicker().setMaxDate(c.getTimeInMillis());
+            return  dialog;
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+        }
+    }
+
+}
